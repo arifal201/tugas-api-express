0260a31d8a5fe019529f0d441e31e84e5b2addcb
Zainal Mustofa <zainalmustof@gmail.com>
2019-11-30 00:47:50 +0700

    add cart

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 const port = process.env.PORT || 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
+//add the router
+app.use(express.static(__dirname + '/View'));
+app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
